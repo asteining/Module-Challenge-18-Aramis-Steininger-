2a915ce041fa7a3520bb712e531fa558e0ed354b
Aramis Steininger <aramissteininger@gmail.com>
2025-03-07 09:29:31 -0500

    another-config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+    allowedHosts: ['module-challenge-18-aramis-steininger.onrender.com'],
     port: 3000, // Change this to a different port, e.g., 3000
     open: true,
     proxy: {
